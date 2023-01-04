d87e7d1ec129b81689b39c8113fce611867a2d0a
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2023-01-04 18:36:27 -0500

    added first version of homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        sm: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "24px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
