01366444fcee065e3538f701528f2cf8fbea44c4
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2023-06-06 14:13:07 -0400

    fixing carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -36,10 +36,10 @@ const Carousel = (props: PropType) => {
         <div className="embla__container grid auto-cols-[100%] grid-flow-col">
           {images.map((image, index) => (
             <div className="embla__slide flex justify-center" key={index}>
-              <div className="absolute h-[50rem] lg:h-image w-full bg-gradient-to-b from-red to-white opacity-40" />
+              <div className="absolute h-image w-full bg-gradient-to-b from-red to-white opacity-40" />
               <Image
                 priority
-                className="h-[50rem] lg:h-image object-cover"
+                className="h-image object-cover"
                 src={image}
                 alt="Whiz Kids Cover"
               />
@@ -47,13 +47,13 @@ const Carousel = (props: PropType) => {
           ))}
         </div>
         <button
-          className="embla__prev z-100 absolute left-0 top-[35%] lg:top-[50%] hidden transition duration-300 ease-in-out hover:scale-[200%] md:block"
+          className="embla__prev z-100 absolute left-0 top-[50%] hidden transition duration-300 ease-in-out hover:scale-[200%] md:block"
           onClick={scrollPrev}
         >
           <IconChevronLeft className="h-20 w-20 text-white" />
         </button>
         <button
-          className="embla__next z-100 absolute right-0 top-[35%] lg:top-[50%] hidden transition duration-300 ease-in-out hover:scale-[200%] md:block"
+          className="embla__next z-100 absolute right-0 top-[50%] hidden transition duration-300 ease-in-out hover:scale-[200%] md:block"
           onClick={scrollNext}
         >
           <IconChevronRight className="h-20 w-20 text-white" />
--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -24,9 +24,9 @@ const Teams: NextPageWithLayout = () => {
 
   return (
     <>
-      <div className="flex h-adjusted w-full justify-center bg-dark-gray">
-        <div className="card w-full overflow-scroll rounded-none bg-white p-2 shadow-xl md:my-20 md:w-auto md:rounded-xl">
-          <div className="card-body flex flex-col justify-center">
+      <div className="flex min-h-adjusted w-full justify-center bg-dark-gray overflow-scroll">
+        <div className="card w-full rounded-none bg-white p-2 shadow-xl py-10 md:my-auto md:w-auto md:rounded-xl">
+          <div className="card-body flex-col justify-center">
             <h1 className="text-center text-5xl font-bold md:px-24">
               Our <span className="text-red">Teams</span>
             </h1>
