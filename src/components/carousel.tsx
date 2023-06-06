a307f96c60dac46ea6f7bcf992d919d44f2ea14f
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2023-06-05 21:20:20 -0400

    adding inset to fix scrolling issues

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -36,10 +36,10 @@ const Carousel = (props: PropType) => {
         <div className="embla__container grid auto-cols-[100%] grid-flow-col">
           {images.map((image, index) => (
             <div className="embla__slide" key={index}>
-              <div className="absolute h-[50rem] md:h-[100vh] w-full bg-gradient-to-b from-red to-white opacity-40" />
+              <div className="absolute h-[50rem] md:h-[calc(100dvh)] w-full bg-gradient-to-b from-red to-white opacity-40" />
               <Image
                 priority
-                className="h-[50rem] md:h-[100vh] object-cover"
+                className="h-[50rem] md:h-[calc(100dvh)] object-cover"
                 src={image}
                 alt="Whiz Kids Cover"
               />
