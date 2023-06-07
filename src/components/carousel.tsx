a35131e45e7ca5a3e8590253bafe8b54ddb4dc5e
jrasmus6 <89819505+jrasmus6@users.noreply.github.com>
2023-06-07 09:47:10 -0400

    fixing carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -47,13 +47,13 @@ const Carousel = (props: PropType) => {
           ))}
         </div>
         <button
-          className="embla__prev z-100 absolute left-0 top-[50%] hidden transition duration-300 ease-in-out hover:scale-[200%] lg:block"
+          className="embla__prev z-100 absolute left-0 top-[20rem] md:top-[30rem] hidden transition duration-300 ease-in-out hover:scale-[200%] lg:block"
           onClick={scrollPrev}
         >
           <IconChevronLeft className="h-20 w-20 text-white" />
         </button>
         <button
-          className="embla__next z-100 absolute right-0 top-[50%] hidden transition duration-300 ease-in-out hover:scale-[200%] lg:block"
+          className="embla__next z-100 absolute right-0 top-[20rem] md:top-[30rem] hidden transition duration-300 ease-in-out hover:scale-[200%] lg:block"
           onClick={scrollNext}
         >
           <IconChevronRight className="h-20 w-20 text-white" />
